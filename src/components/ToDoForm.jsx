4d55accd55b06e33dfe43c19303201103f4c836f
Danny <nguyencaodat270504@gmail.com>
2024-04-11 15:46:21 -0600

    Work on Fetching Data

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -1,8 +1,14 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import tasksData from '../data/tasks.json';
 
 export default function ToDoForm({addTask}) {
   const [input, setInput] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(tasksData.tasks);
+  }, []);
 
   const handleChangeText = text => {
     setInput(text);
@@ -12,6 +18,12 @@ export default function ToDoForm({addTask}) {
     addTask(input);
     setInput('');
   };
+
+  const handleAddTask = () => {
+    const randomIndex = Math.floor(Math.random() * tasks.length);
+    setInput(tasks[randomIndex]);
+  };
+
   return (
     <View>
       <TextInput
@@ -21,6 +33,11 @@ export default function ToDoForm({addTask}) {
         value={input}
       />
       <Button title="Add Task" onPress={handlePress} style={styles.btn} />
+      <Button
+        title="Generate Random Task"
+        onPress={handleAddTask}
+        style={styles.btn}
+      />
     </View>
   );
 }
@@ -29,6 +46,14 @@ const styles = StyleSheet.create({
   input: {
     backgroundColor: '#CDECFC',
     fontSize: 20,
+    paddingLeft: 15,
+  },
+  taskList: {
+    marginTop: 20,
+  },
+  textData: {
+    fontSize: 17,
+    fontWeight: 'bold',
     paddingLeft: 15,
   },
 });
